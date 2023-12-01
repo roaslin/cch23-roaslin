fe2d8e5036141b0b118ea86b051ba8f8b696ee40
roaslin <roaslin@gmail.com>
2023-12-01 15:10:04 +0100

    Day 1: Packet exclusive-cube not found

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
+}
+
+async fn calculate_sled_id(Path(packets_ids): Path<String>) -> String {
+    let sled_id = packets_ids
+        .split("/")
+        .into_iter()
+        .map(|packet_id| packet_id.parse::<i32>().unwrap())
+        .reduce(|sled_id, packet_id| sled_id ^ packet_id)
+        .unwrap();
+
+    format!("{}", sled_id.pow(3))
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(StatusCode::INTERNAL_SERVER_ERROR));
+        .route("/-1/error", get(StatusCode::INTERNAL_SERVER_ERROR))
+        .route("/1/*packets_ids", get(calculate_sled_id));
 
     Ok(router.into())
 }
