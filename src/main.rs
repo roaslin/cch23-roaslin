b59e49afb3002c53d85fa7161186438826f8029b
roaslin <roaslin@gmail.com>
2023-12-01 15:46:19 +0100

    Day 1: Packet exclusive-cube not found handle 20 packets only

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use axum::{extract::Path, http::StatusCode, routing::get, Router};
 
+static MAX_PACKETS_IN_SLED: usize = 20;
+
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
-async fn calculate_sled_id(Path(packets_ids): Path<String>) -> String {
-    let sled_id = packets_ids
-        .split("/")
+async fn calculate_sled_id(Path(packets_ids): Path<String>) -> (StatusCode, String) {
+    let sled_id: Vec<&str> = packets_ids.split("/").collect();
+
+    if sled_id.len() > MAX_PACKETS_IN_SLED {
+        return (
+            StatusCode::BAD_REQUEST,
+            String::from("Ho Ho ho!! Only 20 packets fit in a sled!"),
+        );
+    }
+
+    let sled_id = sled_id
         .into_iter()
         .map(|packet_id| packet_id.parse::<i32>().unwrap())
         .reduce(|sled_id, packet_id| sled_id ^ packet_id)
         .unwrap();
 
-    format!("{}", sled_id.pow(3))
+    (StatusCode::OK, format!("{}", sled_id.pow(3)))
 }
 
 #[shuttle_runtime::main]
